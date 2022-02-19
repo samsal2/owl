b59ee8d2e76fd0a090265ee3f779fee6ff3379b8
samsal2 <samsal9681@gmail.com>
2022-02-19 03:53:41 -0600

    [internal]: fix OWL_STATIC_ASSET

--- owl/internal.h
+++ owl/internal.h
@@ -50,7 +50,7 @@ void owl_dbg_log_(char const *f, int l, char const *fmt, ...);
 #define OWL_ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #define OWL_ALIGNU2(v, a) ((v) + (a)-1) & ~((a)-1)
 #define OWL_STATIC_ASSERT(b, msg)                                              \
-  typedef char owl_static_assert_##__LINE__##_[!!b]
+  typedef char owl_static_assert_##__LINE__##_[!!(b)]
 
 #ifndef NDEBUG
 
